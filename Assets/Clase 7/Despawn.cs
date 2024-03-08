e515a2529344451cd896c13e675ced664aa37784
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2024-03-08 17:15:08 -0600

    Effects
    Cutres pero sirven

--- Assets/Clase 7/Despawn.cs
+++ Assets/Clase 7/Despawn.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Despawn : MonoBehaviour
 {
+    public GameObject particles;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
-            Destroy(this.gameObject);
+            particles.gameObject.SetActive(true);
+
+            StartCoroutine(DestroyCoroutine());
         }
+    }
+
+    IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(this.gameObject);
     }
 }
